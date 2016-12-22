746370e6696b56fe5ca25998960901e8512c8e89
Batsal Awaley <batsal@vayu.com.au>
2016-12-22 14:54:33 +0545

    Complete basic login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  root                    'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
+  get    '/help',     to: 'static_pages#help'
 
-  get '/about', to: 'static_pages#about'
+  get    '/about',    to: 'static_pages#about'
 
-  get '/contact', to: 'static_pages#contact'
+  get    '/contact',  to: 'static_pages#contact'
 
-  get '/signup', to: 'users#new'
+  get    '/signup',   to: 'users#new'
 
-  post '/signup', to: 'users#create'
+  post   '/signup',   to: 'users#create'
 
-  get '/login', to: 'sessions#new'
+  get    '/login',    to: 'sessions#new'
 
-  post '/login', to: 'sessions#create'
+  post   '/login',    to: 'sessions#create'
 
-  delete '/logout', to: 'sessions#destroy'
+  delete '/logout',   to: 'sessions#destroy'
 
   resources :users
 
