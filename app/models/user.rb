087668bcfadf1e957b4d80085c60cecfde8a50d7
Batsal Awaley <batsal@vayu.com.au>
2016-12-23 13:34:43 +0545

    Successful edits with TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  validates :password, length: { minimum: 6 }, presence: true
+  # validates :password, length: { minimum: 6 }, presence: true
 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string
   def User.digest(string)
