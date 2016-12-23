11d50a3095ad0331e020f793240acdd85d5e0a53
Batsal Awaley <batsal@vayu.com.au>
2016-12-23 10:39:42 +0545

    Test the remember branch.

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class UserLoginTest < ActionDispatch::IntegrationTest
 
   def setup
@@ -35,5 +34,19 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: 1)
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # Log in to set cookie.
+    log_in_as(@user, remember_me: 1)
+    #Log in to verify that the cookie is deleted.
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies['remember_token']
   end
 end
