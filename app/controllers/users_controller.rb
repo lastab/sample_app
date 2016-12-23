e3c1823fac9b4102d3dbe345e54a384a5dae9284
Batsal Awaley <batsal@vayu.com.au>
2016-12-23 12:13:45 +0545

    add feature to handel unsuccessful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      #Handle a successful update.
     else
       render 'edit'
     end
