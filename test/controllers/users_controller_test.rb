0b744896de99fac6b418e77953d530ab28065936
Batsal Awaley <batsal@vayu.com.au>
2016-12-23 14:53:44 +0545

    Requesting the right user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,11 +3,19 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
     get new_user_url
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
   test "should redirect update when not logged in" do
